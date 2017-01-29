0dce30cfb797beb56d03f91d384f273c1731a81b
Anton <antonball@gmail.com>
2017-01-29 22:22:46 +0800

    Added coloured die and assets

--- Dice5/ViewController.swift
+++ Dice5/ViewController.swift
@@ -13,9 +13,17 @@ class ViewController: UIViewController {
     let die20:Dice = Dice(sides: 20, color: "red")
 
     @IBOutlet weak var d20: UIButton!
+    @IBOutlet weak var dieBackground: UIImageView!
 
     @IBAction func d20Tapped(_ sender: Any) {
         d20.setImage(UIImage(named: die20.rollImage()), for: .normal)
+        dieBackground.image = dieBackground.image!.withRenderingMode(.alwaysTemplate)
+        dieBackground.tintColor = UIColor(
+            hue: CGFloat(Float(arc4random_uniform(360))/360),
+            saturation: CGFloat(min(Float(arc4random_uniform(100))/100 + 0.2, 1.0)),
+            brightness: CGFloat(min(Float(arc4random_uniform(100))/100 + 0.2, 1.0)),
+            alpha: 1.0
+        )
     }
 
 }
