f9245b28b4c291c26d700b62264a4cff7be9d780
James van der Moezel <jamesvdm@me.com>
2017-03-07 18:49:18 +0800

    Added stack views to widget

--- Dice5Widget/TodayViewController.swift
+++ Dice5Widget/TodayViewController.swift
@@ -14,6 +14,11 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     let die20:Dice = Dice(sides: 20, color: "red")
     let die4:Dice = Dice(sides: 4, color: "red")
     let die6:Dice = Dice(sides: 6, color: "red")
+    let die8:Dice = Dice(sides: 8, color: "red")
+    let die10:Dice = Dice(sides: 10, color: "red")
+    let die12:Dice = Dice(sides: 12, color: "red")
+    let die100:Dice = Dice(sides: 10, color: "red")
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +39,10 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     @IBOutlet weak var d20: UIButton!
     @IBOutlet weak var d4: UIButton!
     @IBOutlet weak var d6: UIButton!
+    @IBOutlet weak var d10: UIButton!
+    @IBOutlet weak var d12: UIButton!
+    @IBOutlet weak var d100: UIButton!
+    @IBOutlet weak var d8: UIButton!
 
     
     override func didReceiveMemoryWarning() {
@@ -56,12 +65,29 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     }
 
     @IBAction func d4Tapped(_ sender: Any) {
-           d4.setImage(UIImage(named: die4.rollImage()), for: .normal)
+        d4.setImage(UIImage(named: die4.rollImage()), for: .normal)
     }
     
     @IBAction func d6Tapped(_ sender: Any) {
-            d6.setImage(UIImage(named: die6.rollImage()), for: .normal)
+        d6.setImage(UIImage(named: die6.rollImage()), for: .normal)
     }
 
+    @IBAction func d8Tapped(_ sender: Any) {
+        d8.setImage(UIImage(named: die8.rollImage()), for: .normal)
+    }
+    
+    @IBAction func d10Tapped(_ sender: Any) {
+        d10.setImage(UIImage(named: die10.rollImage()), for: .normal)
+    }
+    
+    @IBAction func d12Tapped(_ sender: Any) {
+        d12.setImage(UIImage(named: die12.rollImage()), for: .normal)
+    }
+    
+    @IBAction func d100Tapped(_ sender: Any) {
+        let d100Roll = String(arc4random_uniform(10))
+        d100.setImage(UIImage(named: "d100_\(d100Roll)"), for: .normal)
+
+    }
     
 }
